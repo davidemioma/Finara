fc9398931ee1ef607c15a4950b3c9b36c4596f20
david emioma <ue22aac@herts.ac.uk>
2024-06-25 17:08:29 +0100

    App setup

---
+++ client/vite.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dir, "./src"),
+      "@components": path.resolve(import.meta.dir, "./src/components"),
+      "@server": path.resolve(import.meta.dir, "../server"),
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
